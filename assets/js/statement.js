379c135e5cdd167f320a3b98b0867e3e388eb4bc
paixaols <lucas.lsop@gmail.com>
2022-09-08 21:18:43 -0300

    Filters in statement page

--- assets/js/statement.js
+++ assets/js/statement.js
@@ -27,10 +27,39 @@ function makeRequest(method, url, data=undefined, callback=undefined) {
     }
 }
 
-function fillTable(response){
+document.getElementById('btn-apply-filters').onclick = function(){
+    var select = document.getElementById('select-menu1')
+    var institutionFilter = select.options[select.selectedIndex].value
+    var select = document.getElementById('select-menu2')
+    var eventFilter = select.options[select.selectedIndex].value
+    var select = document.getElementById('select-menu3')
+    var assetFilter = select.options[select.selectedIndex].value
+    var filteredTransactions = []
+    if(institutionFilter === '0' & eventFilter === '0' & assetFilter === '0'){
+        fillTable(transactionsList)
+    }else{
+        for(i in transactionsList){
+            var t = transactionsList[i]
+            if(institutionFilter != '0' & t.bank != institutionFilter){
+                continue
+            }
+            if(eventFilter != '0' & t.event != eventFilter){
+                continue
+            }            
+            if(assetFilter != '0' & t.asset_name != assetFilter){
+                continue
+            }
+            filteredTransactions.push(t)
+        }
+        fillTable(filteredTransactions)
+    }
+}
+
+function fillTable(transactions){
     let table = document.getElementById('statement-table-body')
-    for(i in response){
-        var entry = response[i]
+    table.innerText = ''
+    for(i in transactions){
+        var entry = transactions[i]
         var html = `<tr><td>${Number(i)+1}</td>`
         html += `<td>${dateFormat.format(new Date(entry.date))}</td>`
         html += `<td>${entry.bank}</td>`
@@ -47,10 +76,53 @@ function fillTable(response){
     }
 }
 
+let transactionsList = undefined
+
+function callback(response){
+    transactionsList = response
+    let institutionList = []
+    let eventList = []
+    let assetList = []
+    for(i in transactionsList){
+        var transaction = transactionsList[i]
+        if(!institutionList.includes(transaction.bank)){
+            institutionList.push(transaction.bank)
+        }
+        if(!eventList.includes(transaction.event)){
+            eventList.push(transaction.event)
+        }
+        if(!assetList.includes(transaction.asset_name)){
+            assetList.push(transaction.asset_name)
+        }
+    }
+    institutionList.sort()
+    var html = ''
+    for(i in institutionList){
+        html += `<option value="${institutionList[i]}">${institutionList[i]}</option>`
+    }
+    document.getElementById('select-menu1').insertAdjacentHTML('beforeend', html)
+
+    eventList.sort()
+    var html = ''
+    for(i in eventList){
+        html += `<option value="${eventList[i]}">${eventList[i]}</option>`
+    }
+    document.getElementById('select-menu2').insertAdjacentHTML('beforeend', html)
+
+    assetList.sort()
+    var html = ''
+    for(i in assetList){
+        html += `<option value="${assetList[i]}">${assetList[i]}</option>`
+    }
+    document.getElementById('select-menu3').insertAdjacentHTML('beforeend', html)
+
+    fillTable(transactionsList)
+}
+
 let dateFormat = new Intl.DateTimeFormat('pt', {year: 'numeric', month: 'short', day: '2-digit'})
 let currencyFormat = new Intl.NumberFormat('pt', {style: 'currency', currency: 'BRL'})
 
 function onLoad() {
-    makeRequest('GET', apiURL+'/get-full-statement', undefined, fillTable)
+    makeRequest('GET', apiURL+'/get-full-statement', undefined, callback)
 }
 document.addEventListener('load', onLoad())
